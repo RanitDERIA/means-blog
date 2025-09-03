900a465422eec184f5d0bf9bbe47516d8627d13f
Ranit Deria <121992255+RanitDERIA@users.noreply.github.com>
2025-09-03 15:28:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,12 +52,7 @@ export default function Home({ blogs, topics }) {
           property="twitter:image"
           content="https://raw.githubusercontent.com/RanitDERIA/means-blog/main/Extra/sc.png"
         />
-        
-        {/* Favicons - Note the leading slash */}
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      
       </Head>
       
       <div className="min-h-screen relative bg-white dark:bg-gray-900">
